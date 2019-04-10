ec857f28f0e718f35ecef297d83192eeaa0071bb
ranjithashavi <49471266+ranjithashavi@users.noreply.github.com>
2019-04-10 17:21:19 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>  
 int main()
 {
-  for(n=0;n<=10;n++)
-  {
     if (n%2 == 0)
         printf("Even\n");
      else
         printf("Odd\n");
-  }    
    return 0;
 }
